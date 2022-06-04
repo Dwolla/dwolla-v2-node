ce980f7b6f27cfb8e6ec7fd24a71e23d2bcf0030
James Coon <james@jcoon.dev>
2022-06-04 01:52:48 -0400

    Add Jest test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+    collectCoverage: true,
+    collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!<rootDir>/src/**/*.d.ts"],
+    coverageDirectory: "<rootDir>/coverage",
+    rootDir: "./",
     preset: "ts-jest",
     testEnvironment: "node",
     testMatch: ["**/tests/**/*.(spec|test).ts", "!**/(build|bundle)/**/*"]
