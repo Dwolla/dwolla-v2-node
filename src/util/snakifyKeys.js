a5270a0bf46a98cb5c0402f718a628c1cf5371b1
James Coon <james@jcoon.dev>
2022-09-29 15:30:22 -0400

    Inline dependency on `lodash.snakecase` package

--- src/util/snakifyKeys.js
+++ src/util/snakifyKeys.js
@@ -1,11 +1,12 @@
-var snakecase = require("lodash.snakecase");
+const _toSnakeCase = str =>
+  str.replace(/[A-Z]/g, char => `_${char.toLowerCase()}`);
 
 module.exports = function snakifyKeys(obj) {
   if (typeof obj === "object") {
     var keys = Object.keys(obj);
     for (var i = 0; i < keys.length; i++) {
       var k = keys[i];
-      var snaked = snakecase(k);
+      var snaked = _toSnakeCase(k);
       if (k !== snaked) {
         var v = obj[k];
         delete obj[k];
